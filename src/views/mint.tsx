768d3132fbb083bfcb1188613ed92f8a1c5c6b4b
BWYC <129849126+BWYC@users.noreply.github.com>
2023-10-11 20:05:14 +0000

    Update mint.tsx

--- src/views/mint.tsx
+++ src/views/mint.tsx
@@ -64,135 +64,130 @@ const Mint: NextPage = () => {
   };
 
   return (
-    <>
-      <div style={{ marginBottom: "10%" }}>
-        <NAVI rootClassName="navi-root-class-name"></NAVI>
-        <hr />
-        <hr />
-        <div className={styles.container}>
-          <main className={styles.main}>
-            {!isContractMetadataLoading && (
-              <div className={styles.heroSection}>
-                <div>
-                  <h4 style={{ textAlign: "center" }}>
-                    {contractMetadata.name}
-                  </h4>
-                  <hr />
-                  <p style={{ fontStyle: "italic" }}>
-                    {contractMetadata.description}
-                  </p>
-                  <hr />
-                  {!isActiveClaimPhaseLoading ? (
-                    <div>
-                      <p>
-                        PHASE:
-                        <p style={{ color: "orange", fontSize: "20px" }}>
-                          {activeClaimPhase?.metadata?.name}
-                        </p>
-                      </p>
-                      <hr />
-                      <p>
-                        Price:{" "}
-                        <p style={{ color: "orange", fontSize: "20px" }}>
-                          {ethers.utils.formatUnits(activeClaimPhase?.price!)}{" "}
-                          CORE
-                        </p>
-                      </p>
-                      <hr />
-                    </div>
-                  ) : (
-                    <p>Loading...</p>
-                  )}
-                  {!isTotalSupplyLoading && !isTotalClaimSupplyLoading ? (
+    <div>
+      <NAVI rootClassName="navi-root-class-name"></NAVI>
+      <hr />
+      <hr />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          {!isContractMetadataLoading && (
+            <div className={styles.heroSection}>
+              <div>
+                <h4 style={{ textAlign: "center" }}>{contractMetadata.name}</h4>
+                <hr />
+                <p style={{ fontStyle: "italic" }}>
+                  {contractMetadata.description}
+                </p>
+                <hr />
+                {!isActiveClaimPhaseLoading ? (
+                  <div>
                     <p>
-                      Claimed: <hr />
+                      PHASE:
                       <p style={{ color: "orange", fontSize: "20px" }}>
-                        {totalClaimSupply?.toNumber()} /{" "}
-                        {totalSupply?.toNumber()}
+                        {activeClaimPhase?.metadata?.name}
                       </p>
-                      <hr />
-                      <ProgressBar
-                        maxCompleted={totalSupply?.toString() / 10}
-                        bgColor="orange"
-                        labelClassName="label"
-                        completed={totalClaimSupply?.toString() / 10}
-                      />
                     </p>
-                  ) : (
-                    <p>Loading...</p>
-                  )}
-                  {address ? (
-                    !isClaimIneligibilityReasonsLoading ? (
-                      claimIneligibilityReasons?.length! > 0 ? (
-                        claimIneligibilityReasons?.map((reason, index) => (
-                          <p style={{ margin: "10px" }} key={index}>
-                            {reason}
-                          </p>
-                        ))
-                      ) : (
-                        <div>
-                          <hr />
-                          <p style={{ color: "gray" }}>
-                            YOU ARE Eligible TO MINT SII WHALES
-                          </p>
-                          <hr stlye={{ color: "white" }} />
-                          <div className={styles.claimContainer}>
-                            <div className={styles.claimValue}>
-                              <button
-                                className={styles.claimBtn}
-                                onClick={decrement}
-                              >
-                                -
-                              </button>
-                              <input
-                                className={styles.claimInput}
-                                type="number"
-                                value={claimQuantity}
-                                placeHolder="Amount"
-                              />
-                              <button
-                                className={styles.claimBtn1}
-                                onClick={increment}
-                              >
-                                +
-                              </button>
-                            </div>
-                            <hr />
-                            <Web3Button
-                              style={{
-                                color: "orange",
-                                background: "black",
-                                textAlign: "center",
-                              }}
-                              contractAddress={
-                                "0x98E8B58c44e3c7f08171bb57aeD010fDF71B351E"
-                              }
-                              action={(contract) =>
-                                contract.erc721.claim(claimQuantity)
-                              }
-                              onSuccess={() => `/profile/${address}`}
-                            >
-                              MINT SEAON II
-                            </Web3Button>
-                          </div>
-                        </div>
-                      )
+                    <hr />
+                    <p>
+                      Price:{" "}
+                      <p style={{ color: "orange", fontSize: "20px" }}>
+                        {ethers.utils.formatUnits(activeClaimPhase?.price!)}{" "}
+                        CORE
+                      </p>
+                    </p>
+                    <hr />
+                  </div>
+                ) : (
+                  <p>Loading...</p>
+                )}
+                {!isTotalSupplyLoading && !isTotalClaimSupplyLoading ? (
+                  <p>
+                    Claimed: <hr />
+                    <p style={{ color: "orange", fontSize: "20px" }}>
+                      {totalClaimSupply?.toNumber()} / {totalSupply?.toNumber()}
+                    </p>
+                    <hr />
+                    <ProgressBar
+                      maxCompleted={totalSupply?.toString() / 10}
+                      bgColor="orange"
+                      labelClassName="label"
+                      completed={totalClaimSupply?.toString() / 10}
+                    />
+                  </p>
+                ) : (
+                  <p>Loading...</p>
+                )}
+                {address ? (
+                  !isClaimIneligibilityReasonsLoading ? (
+                    claimIneligibilityReasons?.length! > 0 ? (
+                      claimIneligibilityReasons?.map((reason, index) => (
+                        <p style={{ margin: "10px" }} key={index}>
+                          {reason}
+                        </p>
+                      ))
                     ) : (
-                      <p>Checking Eligibility...</p>
+                      <div>
+                        <hr />
+                        <p style={{ color: "gray" }}>
+                          YOU ARE Eligible TO MINT SII WHALES
+                        </p>
+                        <hr stlye={{ color: "white" }} />
+                        <div className={styles.claimContainer}>
+                          <div className={styles.claimValue}>
+                            <button
+                              className={styles.claimBtn}
+                              onClick={decrement}
+                            >
+                              -
+                            </button>
+                            <input
+                              className={styles.claimInput}
+                              type="number"
+                              value={claimQuantity}
+                              placeHolder="Amount"
+                            />
+                            <button
+                              className={styles.claimBtn1}
+                              onClick={increment}
+                            >
+                              +
+                            </button>
+                          </div>
+                          <hr />
+                          <Web3Button
+                            style={{
+                              color: "orange",
+                              background: "black",
+                              textAlign: "center",
+                            }}
+                            contractAddress={
+                              "0x98E8B58c44e3c7f08171bb57aeD010fDF71B351E"
+                            }
+                            action={(contract) =>
+                              contract.erc721.claim(claimQuantity)
+                            }
+                            onSuccess={() => `/profile/${address}`}
+                          >
+                            MINT SEAON II
+                          </Web3Button>
+                        </div>
+                      </div>
                     )
                   ) : (
-                    <p>Connect wallet</p>
-                  )}
-                  <div></div>
-                </div>
+                    <p>Checking Eligibility...</p>
+                  )
+                ) : (
+                  <p>Connect wallet</p>
+                )}
+                <div></div>
               </div>
-            )}
-            <hr />
-            <Profile />
-          </main>
-        </div>
+            </div>
+          )}
+          <hr />
+          <Profile />
+        </main>
       </div>
-    </>
+    </div>
   );
 };
 
