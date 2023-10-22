11d6cf5e71eb4c634a76e4931486428a48790752
BWYC <129849126+BWYC@users.noreply.github.com>
2023-10-22 12:34:36 +0000

    Update mems.css and mems.js

--- src/components/members/mems.js
+++ src/components/members/mems.js
@@ -4,10 +4,14 @@ import "./mems.css";
 
 export default function Members({ data }) {
   return (
-    <ul className="item-wrapper">
-      {data.map((row) => (
-        <Item row={row} key={row.userID} />
-      ))}
-    </ul>
+    <>
+      <h3>SII LEADERBOARD</h3>
+      <hr />
+      <ul className="item-wrapper">
+        {data.map((row) => (
+          <Item row={row} key={row.userID} />
+        ))}
+      </ul>
+    </>
   );
 }
